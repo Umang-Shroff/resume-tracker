5cf578060c7eee87b357cdcea5e6f4d2190aa0e2
Umang <soyumang55@gmail.com>
2025-01-23 12:40:50 +0530

    extracting text

--- app.py
+++ app.py
@@ -7,3 +7,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+
+def get_gemini_response(input):
+    model=genai.GenerativeModel('gemini-pro')
+    response=model.generate_content(input)
+    return response.text
+
+def input_pdf_text(file):
+    reader=pdf.PdfFileReader(file)
+    text=""
+    for page in reader(len(reader.pages)):
+        page=reader.pages[page]
+        text+=str(page.extract_text())
